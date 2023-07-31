382bcee3a8a423bae9c8c678992c495677499e57
Emil Kovacevic <77700903+emilkovacevic@users.noreply.github.com>
2023-07-31 20:55:12 +0200

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Ecommerce app
+
+Full-stack e-commerce app built with Next.js.
+
+![Ecommerce Website](./cover-ecomm.PNG)
+![Ecommerce Product](./product.PNG)
+## Features:
+
+- User Login and new Account creation with credentials
+- Search bar filtering
+- Tags Filtering
+- Categories Filtering
+- Wishlist page
+- Cart page with checkout
+- Dark and light color modes
+- Ability to add/remove and list items when logged in by manually navigating to `baseurl/admin`
+
+## To run locally:
+
+1. Create a .env file and insert the following: 
+
+```shell
+DATABASE_URL=''
+NEXTAUTH_SECRET=""
+JWT_SECRET=''
+```
+
+2. Install dependencies with `npm i`
+
+3. Push the database to your DB host (e.g., Railway) using Prisma
+
+4. Run the development server: `npm run dev`
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -6,6 +6,7 @@ import Header from "@components/Header";
 
 import { ProductProps } from "@components/Product";
 import axiosinstance from "src/axios/instance";
+import Checkbox from "@components/Checkbox";
 
 interface Props {
   product: ProductProps;
@@ -25,14 +26,19 @@ function Product({ product, products }: Props) {
       <header className="flex w-2/3">
         <h1 className="w-full text-left font-black text-2xl text-gray-700 dark:text-white">{product.name}</h1>
       </header>
-      <main className="flex w-2/3 h-fit justify-center gap-8 bg-slate-300 py-10 dark:bg-slate-700">
-        <section className="flex flex-col w-1/3 h-fit">
-          <span className="flex items-center justify-center aspect-square">
-            <img src={product.imageUrl} alt={product.name} title={product.name} />
-          </span>
+      <main className="flex w-2/3 gap-8 ">
+        <section className="max-w-[450px] h-full">
+            <img
+            className="object-fill"
+            src={product.imageUrl} alt={product.name} title={product.name} />
         </section>
         <aside className="flex flex-col w-1/3 justify-between">
+          <div
+          className="flex flex-col justify-between pt-4 h-full"
+          >
           <span className="text-sm text-gray-700 dark:text-white font-normal">{product.description}</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod reiciendis non dolores harum vitae unde odio repellendus. Corrupti eos dolorem, et asperiores officia provident laboriosam nihil aliquam commodi tempora error!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum deleniti quaerat praesentium sed voluptatum natus enim doloribus at a beatae magni officiis eum earum, dolores minus explicabo, eveniet quibusdam in.</p>
           <div className="flex my-4 md:mt-0 flex-row flex-wrap w-full items-center gap-4">
             <span className="text-2xl font-black text-gray-700 dark:text-white">${handlePrice(product.price)}</span>
             <button
@@ -42,8 +48,15 @@ function Product({ product, products }: Props) {
               Add to Cart
             </button>
           </div>
+          </div>
         </aside>
       </main>
+      <footer 
+      className="w-2/3"
+      >
+        <h2>Fullstack demo website</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam eveniet repudiandae vero nobis aliquam et nemo eligendi suscipit! Error porro minus repellendus architecto quo reiciendis itaque cum commodi exercitationem inventore.</p>
+      </footer>
     </div>
   );
 }
